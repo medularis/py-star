26f5fb399bb9b8405974233180f261f60f0a6e22
German Larrain <glarrain@users.noreply.github.com>
2014-07-15 11:13:51 -0400

    major cleanup of 'setup.py'
    * set version as `asterisk.__version__` (it was using `None`)
    * remove all the processing crap (of README, description, license,
      download URL)
    * make PEP8 compliant
    
    (cherry picked from commit 68553d55e78a5db06e5871db19e3b32a2aed830a)

--- setup.py
+++ setup.py
@@ -2,53 +2,44 @@
 
 from distutils.core import setup
 
-try :
-    from asterisk.Version  import VERSION
-except :
-    VERSION = None
+import asterisk
 
-description = []
-f = open ('README.rst')
-logo_stripped = False
-for line in f :
-    if not logo_stripped and line.strip () :
-        continue
-    logo_stripped = True
-    description.append (line)
 
-licenses = ( 'Python Software Foundation License'
-           , 'GNU Library or Lesser General Public License (LGPL)'
-           )
-download = "http://downloads.sourceforge.net/project/pyst/pyst"
+long_description = (
+    "Pyst consists of a set of interfaces and libraries to allow programming "
+    "of Asterisk from python. The library currently supports AGI, AMI, "
+    "and the parsing of Asterisk configuration files. The library also "
+    "includes debugging facilities for AGI."
+)
 
-setup \
-    ( name = 'pyst'
-    , version = VERSION
-    , description = 'A Python Interface to Asterisk'
-    , long_description = ''.join (description)
-    , author = 'Karl Putland'
-    , author_email = 'kputland@users.sourceforge.net'
-    , maintainer = 'Ralf Schlatterbeck'
-    , maintainer_email = 'rsc@runtux.com'
-    , url = 'http://www.sourceforge.net/projects/pyst/'
-    , packages = ['asterisk']
-    , license = ', '.join (licenses)
-    , platforms = 'Any'
-    , download_url = \
-        "%(download)s/%(VERSION)s/pyst-%(VERSION)s.tar.gz" % locals ()
-    , classifiers =
-        [ 'Development Status :: 5 - Production/Stable'
-        , 'Environment :: Other Environment'
-        , 'Intended Audience :: Developers'
-        , 'Intended Audience :: Telecommunications Industry'
-        , 'Operating System :: OS Independent'
-        , 'Programming Language :: Python'
-        , 'Programming Language :: Python :: 2.4'
-        , 'Programming Language :: Python :: 2.5'
-        , 'Programming Language :: Python :: 2.6'
-        , 'Programming Language :: Python :: 2.7'
-        , 'Topic :: Communications :: Internet Phone'
-        , 'Topic :: Communications :: Telephony'
-        , 'Topic :: Software Development :: Libraries :: Python Modules'
-        ] + ['License :: OSI Approved :: ' + l for l in licenses]
-    )
+setup(
+    name='pyst',
+    version=asterisk.__version__,
+    description='A Python Interface to Asterisk',
+    long_description=long_description,
+    author='Karl Putland',
+    author_email='kputland@users.sourceforge.net',
+    maintainer='Ralf Schlatterbeck',
+    maintainer_email='rsc@runtux.com',
+    url='http://www.sourceforge.net/projects/pyst/',
+    packages=['asterisk'],
+    license='PSF, LGPL',
+    platforms='Any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Internet Phone',
+        'Topic :: Communications :: Telephony',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    ]
+)
